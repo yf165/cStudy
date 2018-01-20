6c604a9d41d90ac7861afafd3290b4393df2b84e
yf165 <35261313+yf165@users.noreply.github.com>
2018-01-20 20:04:58 +0800

    Rev 2.0
    static library

---
+++ staticLibrary/test/test.c
@@ -0,0 +1,13 @@
+#include "yf.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int sum =0;
+	
+	func1();
+	sum = func2(1,2);
+	printf("1+2 = %d\n",sum);
+	return 0;
+	
+}
---
+++ staticLibrary/yf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void func1(void)
+{
+	printf("func1 in lib yf\n");
+}
+
+int func2(int a,int b)
+{
+	printf("func2 in lib yf\n");
+	return a+b;
+}
